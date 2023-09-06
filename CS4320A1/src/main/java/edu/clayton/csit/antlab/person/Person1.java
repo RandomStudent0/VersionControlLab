21cc0c4c30725bec0b9ce799a46affdac231d391
RandomStudent0 <89224410+RandomStudent0@users.noreply.github.com>
2023-09-06 17:50:03 -0400

    Update Person1.java
    I was assigned to Person1. Here is my implementation of the code.

--- CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person1.java
+++ CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person1.java
@@ -1,49 +1,32 @@
 package edu.clayton.csit.antlab.person;
 
-/**
- *  A simple class for person 1
- *  returns their name and a
- *  modified string 
- *  
- *  @author Qu
- *  @version 1.1
- */
 public class Person1 {
-  /** Holds the persons real name */
-  private String name;
-  	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-  public Person1(String pname) {
-    name = pname;
-  }
-  	/**
-	 * This method should take the string
-	 * input and return its characters rotated
-	 * 2 positions.
-	 * given "gtg123b" it should return
-	 * "g123bgt".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
+	//Holds the person's unedited name
+	private String name; 
+
+  	//Person1 constructor
+	public Person1(String pname) {
+     	name = pname;
+    	}
+  	
+	// Method takes a string 'input' and rotates the first two
+	// letters to the end. Ex: first --> rstfi
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		String rotated = "";
+		String temp = "";
+		for (int i = 0; i < input.length(); i++){
+			if (i < 2)
+				temp = temp + input.charAt(i);
+			else
+				rotated = rotated + input.charAt(i);
+		}
+		rotated = rotated + temp;
+		return rotated;
 	}
 	
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
+	//Return a string rep of this object
+	//that varies with an input string
 	public String toString(String input) {
 	  return name + calc(input);
 	}
-
 }
